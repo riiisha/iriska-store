6dfed81098f59d392cce02e775311f4a7a4028b6
iriska <ibortnyak@corp.local>
2024-11-24 21:42:40 +0300

    add repositories

---
+++ src/Repository/CartItemRepository.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\CartItem;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<CartItem>
+ */
+class CartItemRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, CartItem::class);
+    }
+
+}
---
+++ src/Repository/Order/OrderItemRepository.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Repository\Order;
+
+use App\Entity\Order\OrderItem;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<OrderItem>
+ */
+class OrderItemRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, OrderItem::class);
+    }
+}
