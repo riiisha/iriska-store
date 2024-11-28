ebb4d5f287954fc6ce76b3f576a496adfcf60c02
iriska <riiisha.0269@gmail.com>
2024-11-28 13:36:36 +0300

    logging 5xx errors

--- src/EventListener/Exception/ExceptionListener.php
+++ src/EventListener/Exception/ExceptionListener.php
@@ -2,6 +2,7 @@
 
 namespace App\EventListener\Exception;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -9,8 +10,13 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
 
 #[AsEventListener]
-class ExceptionListener
+readonly class ExceptionListener
 {
+    public function __construct(
+        private LoggerInterface $logger
+    ) {
+    }
+
     public function __invoke(ExceptionEvent $event): void
     {
         $exception = $event->getThrowable();
@@ -32,5 +38,9 @@ class ExceptionListener
 
         $response = new JsonResponse($responseContent, $statusCode);
         $event->setResponse($response);
+
+        if ($statusCode >= 500) {
+            $this->logger->error('An error occurred', ['exception' => $exception]);
+        }
     }
 }
