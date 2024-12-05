036a70437b75aa1a0d54c3a0ae2732c3004802f6
iriska <riiisha.0269@gmail.com>
2024-12-06 00:07:48 +0300

    update ExceptionListener

--- src/EventListener/Exception/ExceptionListener.php
+++ src/EventListener/Exception/ExceptionListener.php
@@ -3,23 +3,33 @@
 namespace App\EventListener\Exception;
 
 use Psr\Log\LoggerInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
+use Throwable;
 
 #[AsEventListener]
 readonly class ExceptionListener
 {
     public function __construct(
-        private LoggerInterface $logger
+        private LoggerInterface $logger,
+        private ?ContainerInterface $container = null
     ) {
     }
 
+    /**
+     * @throws Throwable
+     */
     public function __invoke(ExceptionEvent $event): void
     {
         $exception = $event->getThrowable();
+
+        if ($this->container->get('kernel')->isDebug()) {
+           throw $exception;
+        }
 
         $statusCode = Response::HTTP_INTERNAL_SERVER_ERROR;
         $message = 'An error occurred.';
