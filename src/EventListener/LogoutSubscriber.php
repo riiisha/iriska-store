61c12b31d16022eb2159edf2d9837b49f7991a4c
iriska <riiisha.0269@gmail.com>
2024-12-09 21:00:53 +0300

    add logout

---
+++ src/EventListener/LogoutSubscriber.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\EventListener;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Http\Event\LogoutEvent;
+
+readonly class LogoutSubscriber implements EventSubscriberInterface
+{
+    public static function getSubscribedEvents(): array
+    {
+        return [LogoutEvent::class => 'onLogout'];
+    }
+
+    public function onLogout(LogoutEvent $event): void
+    {
+        $response = new JsonResponse([], Response::HTTP_OK);
+        $event->setResponse($response);
+    }
+}
