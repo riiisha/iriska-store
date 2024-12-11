9fb47958538e31835c50c63a1f73fdeb20307c42
iriska <riiisha.0269@gmail.com>
2024-12-11 21:32:40 +0300

    update consumers

---
+++ src/Consumer/AbstractConsumer.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Consumer;
+
+use Psr\Log\LoggerInterface;
+use RdKafka\Message;
+use SimPod\Kafka\Clients\Consumer\ConsumerConfig;
+use SimPod\Kafka\Clients\Consumer\KafkaConsumer;
+use SimPod\KafkaBundle\Kafka\Clients\Consumer\NamedConsumer;
+use SimPod\KafkaBundle\Kafka\Configuration;
+use Throwable;
+
+abstract class AbstractConsumer implements NamedConsumer
+{
+    private const TIMEOUT_MS = 2000;
+
+    public function __construct(
+        readonly Configuration   $configuration,
+        readonly LoggerInterface $logger,
+    ){
+    }
+
+    public function run(): void
+    {
+        $kafkaConsumer = new KafkaConsumer($this->getConfig());
+        $kafkaConsumer->subscribe([$this->getTopicName()]);
+        while (true) {
+            $kafkaConsumer->start(
+                self::TIMEOUT_MS,
+                function (Message $message) use ($kafkaConsumer): void {
+                    try {
+                        $this->processMessage($message);
+                        $kafkaConsumer->commit($message);
+                    } catch (Throwable $exception) {
+                        $this->logger->error("[{$this->getName()}]", [
+                            'message' => $exception->getMessage(),
+                            'exception' => $exception->getTrace()
+                        ]);
+                    }
+                }
+            );
+        }
+    }
+    abstract public function getName(): string;
+
+    abstract protected function getTopicName(): string;
+
+    abstract function processMessage(Message $message): void;
+
+    private function getConfig(): ConsumerConfig
+    {
+        $config = new ConsumerConfig();
+
+        $config->set(ConsumerConfig::BOOTSTRAP_SERVERS_CONFIG, $this->configuration->getBootstrapServers());
+        $config->set(ConsumerConfig::ENABLE_AUTO_COMMIT_CONFIG, false);
+        $config->set(ConsumerConfig::CLIENT_ID_CONFIG, $this->configuration->getClientIdWithHostname());
+        $config->set(ConsumerConfig::AUTO_OFFSET_RESET_CONFIG, 'earliest');
+        $config->set(ConsumerConfig::GROUP_ID_CONFIG, 'consumer_group');
+
+        return $config;
+    }
+}
--- src/Consumer/ProductConsumer.php
+++ src/Consumer/ProductConsumer.php
@@ -4,44 +4,24 @@ namespace App\Consumer;
 
 use App\DTO\Product\CreateProductDTO;
 use App\Service\Product\ProductService;
+use JMS\Serializer\Serializer;
 use JMS\Serializer\SerializerBuilder;
-use JMS\Serializer\SerializerInterface;
+use Psr\Log\LoggerInterface;
 use RdKafka\Message;
-use SimPod\Kafka\Clients\Consumer\ConsumerConfig;
-use SimPod\Kafka\Clients\Consumer\KafkaConsumer;
-use SimPod\KafkaBundle\Kafka\Clients\Consumer\NamedConsumer;
 use SimPod\KafkaBundle\Kafka\Configuration;
 
-final class ProductConsumer implements NamedConsumer
+final class ProductConsumer extends AbstractConsumer
 {
-    private const TIMEOUT_MS = 2000;
-    private Configuration $configuration;
-    private SerializerInterface $serializer;
-    private ProductService $productManager;
+    private Serializer $serializer;
 
     public function __construct(
-        Configuration  $configuration,
-        ProductService $productManager,
-    ) {
-        $this->configuration = $configuration;
-        $this->serializer = (new SerializerBuilder())->build();
-        $this->productManager = $productManager;
-    }
-
-    public function run(): void
+        Configuration           $configuration,
+        LoggerInterface         $logger,
+        readonly ProductService $productManager,
+    )
     {
-        $kafkaConsumer = new KafkaConsumer($this->getConfig());
-        $kafkaConsumer->subscribe(['product_topic']);
-        while (true) {
-            $kafkaConsumer->start(
-                self::TIMEOUT_MS,
-                function (Message $message) use ($kafkaConsumer): void {
-                    $newProduct = $this->serializer->deserialize($message->payload, CreateProductDTO::class, 'json');
-                    $this->productManager->createOrUpdateProduct($newProduct);
-                    $kafkaConsumer->commit($message);
-                }
-            );
-        }
+        parent::__construct($configuration, $logger);
+        $this->serializer = (new SerializerBuilder())->build();
     }
 
     public function getName(): string
@@ -49,16 +29,14 @@ final class ProductConsumer implements NamedConsumer
         return 'product_consumer';
     }
 
-    private function getConfig(): ConsumerConfig
+    protected function getTopicName(): string
     {
-        $config = new ConsumerConfig();
+        return 'product';
+    }
 
-        $config->set(ConsumerConfig::BOOTSTRAP_SERVERS_CONFIG, $this->configuration->getBootstrapServers());
-        $config->set(ConsumerConfig::ENABLE_AUTO_COMMIT_CONFIG, false);
-        $config->set(ConsumerConfig::CLIENT_ID_CONFIG, $this->configuration->getClientIdWithHostname());
-        $config->set(ConsumerConfig::AUTO_OFFSET_RESET_CONFIG, 'earliest');
-        $config->set(ConsumerConfig::GROUP_ID_CONFIG, 'consumer_group');
-
-        return $config;
+    function processMessage(Message $message): void
+    {
+        $newProduct = $this->serializer->deserialize($message->payload, CreateProductDTO::class, 'json');
+        $this->productManager->createOrUpdateProduct($newProduct);
     }
 }
